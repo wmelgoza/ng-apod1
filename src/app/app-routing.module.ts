0e9f1badce01fb272f828923edba7f51eaa818ef
WilliamMelgoza <wmelgoza@protonmail.com>
2020-03-04 23:18:12 -0600

    Add apod routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ApodComponent } from './apod/apod.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/apod', pathMatch: 'full'},
+  { path: 'apod', component: ApodComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
