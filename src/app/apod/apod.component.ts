ffb91d28485520ddd29aab9e243de2feb9a61a21
WilliamMelgoza <wmelgoza@protonmail.com>
2020-03-04 23:13:30 -0600

    Generate an Apod component

---
+++ src/app/apod/apod.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apod',
+  templateUrl: './apod.component.html',
+  styleUrls: ['./apod.component.scss']
+})
+export class ApodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
